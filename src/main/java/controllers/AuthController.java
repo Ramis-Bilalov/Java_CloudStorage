db0f7c51c76d5c00ccd08ef8366027eeb1a7b7b7
Рамис Билалов <ramiska81@gmail.com>
2021-03-21 23:05:47 +0300

    Домашняя работа к 5-му уроку

---
+++ src/main/java/controllers/AuthController.java
@@ -0,0 +1,69 @@
+package controllers;
+
+import Database.UserSQLiteDao;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AuthController implements Initializable {
+    public TextField login;
+    public PasswordField password;
+    TrashController trashController = new TrashController();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void enter(ActionEvent actionEvent) throws IOException {
+        boolean auth = false;
+        try {
+            auth = UserSQLiteDao.getInstance().userExists(login.getText(), password.getText());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (auth) {
+            System.out.println("правильный пароль");
+            Parent client = FXMLLoader.load(getClass().getResource("client.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("CloudStorage - Облачное хранилище данных");
+            stage.setScene(new Scene(client));
+            stage.setResizable(false);
+            stage.show();
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    trashController.closeConnection();
+                }
+            });
+            login.getScene().getWindow().hide();
+        } else {
+            System.out.println("неправильный пароль");
+            login.clear();
+            login.setPromptText("WRONG LOGIN");
+            password.clear();
+            password.setPromptText("WRONG PASSWORD");
+        }
+    }
+
+    public void registrate(ActionEvent actionEvent) throws IOException {
+        Parent reg = FXMLLoader.load(getClass().getResource("registration.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("CloudStorage - Регистрация");
+        stage.setScene(new Scene(reg));
+        stage.setResizable(false);
+        stage.show();
+
+        login.getScene().getWindow().hide();
+    }
+}
